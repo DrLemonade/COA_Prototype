4c02ca4bd799c49e652e437fb7a7294621cf6bfa
DrLemonade <dantealessiajustin@gmail.com>
2024-10-27 04:31:42 -0600

    log in method

--- COA_Project_Prototype/Employee.cs
+++ COA_Project_Prototype/Employee.cs
@@ -1,11 +1,45 @@
 using System;
+using System.Globalization;
+using System.Threading;
+
+public static class Program
+{
+    Employee[] employees;
+    public void main()
+    {
+        employees = {new Executive("executive", "username1", "password"), 
+            new DeptChair("department chair", "username2", "password"), 
+            new ProviderManager("provider manager", "username3", "password"), 
+            new Provider("provider", "username4", "password"),
+            new Analyst("analyst", "username5", "password"),
+            new SiteAdministrator("site administrator", "username6", "password"),
+            new DataAdministrator("data administrator", "username7", "password"),
+            new TenantAdministrator("tenant administrator", "username8", "password")};
+    }
+    
+    public Employee logIn(string username)
+    {
+        int min = 0;
+        int max = employees.length - 1;
+        while(max > min)
+        {
+            mid = (min + max) / 2;
+            if(employees[mid].getUsername().Equals(username))
+                return employees[i];
+            else if(String.Compare(username, employees[i].getUsername(), comparisonType: StringComparison.OrdinalIgnoreCase) < 0)
+                max = mid - 1;
+            else if(String.Compare(username, employees[i].getUsername(), comparisonType: StringComparison.OrdinalIgnoreCase) > 0)
+                min = mid + 1;
+        }
+        return null;
+    }
+}
 
 public abstract class Employee
 {
     private string name;
     private string username;
     private string password;
-    private int employeeID;
 
     public Employee(string name, string username, string password)
     {
