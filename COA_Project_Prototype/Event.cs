14ecef2ac3ae779572666efaffc947883df5a0ab
DrLemonade <dantealessiajustin@gmail.com>
2024-11-18 18:10:04 -0700

    events

--- COA_Project_Prototype/Event.cs
+++ COA_Project_Prototype/Event.cs
@@ -2,7 +2,14 @@
 
 public class Event
 {
-	public Event()
+	public int Costs [][];
+	public DateTime startDate { get; set; }
+	public DateTime endDate { get; set; }
+
+	public Event(startDate, endDate)
 	{
+		Costs = new int[4][6];
+		this.startDate = startDate;
+		this.endDate = endDate;
 	}
 }
